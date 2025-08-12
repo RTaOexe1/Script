c094c51a4d40befd60d6b7961a4b3a917df9bd9c
RTaOexe1 <warutchamchoy789@gmail.com>
2025-08-12 20:21:47 +0700

    Update AutoFramGem.lua

--- AutoFramGem.lua
+++ AutoFramGem.lua
@@ -6,7 +6,7 @@ if getgenv().RunScript == true then	return end
 getgenv().RunScript = true
 
 local queueScript = string.format([[
-loadstring(game:HttpGet('https://raw.githubusercontent.com/dyumra/Detail/refs/heads/main/Somtank'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/RTaOexe1/Script/refs/heads/main/AutoFramGem.lua'))()
 ]])
 
 queue_on_teleport(queueScript)
@@ -269,4 +269,5 @@ else
 	player:Kick()
 	wait(0.5)
 	game:GetService("TeleportService"):Teleport(126509999114328)
+
 end
